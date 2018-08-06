2dee2ce75eb0ff69b5464714bc6d74f48fe06602
Ahmad Khatab Ahmad <ahmadzendane725@gmail.com>
2018-08-06 09:07:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+   document.body.addEventListener("keydown",codes);
+}
+function codes(e)
+{
+   let key = parseInt(e.which ||e.detail);
+   let index =0;
+   
+   if(key ===code[index])
+   {
+     index++;
+     if(index ===code.length-1)
+      alert("Hurray!");
+   }
+   else 
+   index =0;
 }
