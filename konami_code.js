ef9ede95fa8bff6a7d9f23a42c2399d07347b891
Bestun Gardi <bestun95@yahoo.com>
2018-08-09 08:08:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [9,9,9,1];
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+function init()
+{
+let i = 0;
+
+document.body.addeventListener("keydown", function (keypressed){
+  const key = parseInt(keypressed.detail || keypressed.which)
+  if(key === code[i]){
+    i++;
+  
+
+  if(i === code.length){
+    alert("You did it")
+    i = 0;
+  }
+else{
+  i=0
+  
 }
+} 
+});}
